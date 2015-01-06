75b67e93197318c37f553ee9ef3bd7949804d856
tobinfekkes <tobin@camanoislandcoffee.com>
2015-01-05 20:26:11 -0500

    [+] added 'Accounting' menu item

--- woocommmerce-accounting.php
+++ woocommmerce-accounting.php
@@ -7,5 +7,13 @@
 	 	Author URI: http://tobinfekkes.com
 	 	Version: 0.1
 	*/
-	?>
 	
+	
+
+	add_action('admin_menu', "Woocommerce_Accounting");
+
+
+	function Woocommerce_Accounting() {
+		add_menu_page('Accounting', 'Accounting', 'manage_options', 'accounting_dashboard', 'accounting_dashboard', '', $position);
+	}
+?>
