f901f4afc87a90fb1c5bd667b00377dad4ae39e5
tobinfekkes <tobin@camanoislandcoffee.com>
2015-01-05 20:17:17 -0500

    [+] added plugin file, [-] removed readme.txt

---
+++ woocommmerce-accounting.php
@@ -0,0 +1,11 @@
+<?php
+
+	/*
+		Plugin Name: Woocommerce Accounting
+	 	Description: Customized Reports for Subscription Accounting
+	 	Author: tobinfekkes
+	 	Author URI: http://tobinfekkes.com
+	 	Version: 0.1
+	*/
+	?>
+	
